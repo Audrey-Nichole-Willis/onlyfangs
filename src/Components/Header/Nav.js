2712fb7a674ebc815f705fdbb0de9f30e808aa24
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-07-11 14:03:28 -0400

    added a navbar brand and made the navbar turn into a toggalable hamburger menu when it's smaller than large

--- src/Components/Header/Nav.js
+++ src/Components/Header/Nav.js
@@ -2,31 +2,44 @@ import React from "react";
 
 function Nav(){
     return (
-        <nav className="navbar">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Features</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Testimonials</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">FAQ</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Contact</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Login/SignUp</a>
-                    </li>
-                </ul>
-        </nav>
+    <nav className="navbar navbar-expand-lg">
+        <a href="#" className="navbar-brand">OnlyFangs</a>
+        <button 
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#togglingNav"
+            aria-controls="togglingNav"
+            aria-expanded="true"
+            aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars navbar-toggler-icon"></i>
+            </button>
+        <div className="collapse navbar-collapse" id="togglingNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Home</a>
+                </li>
+                <li className="nav-item"> 
+                    <a href="#" className="nav-link"> About</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link"> Features </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Testimonials</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#"> FAQ </a>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Contact</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Login/SignUp </a>
+            </li>
+        </ul>
+        </div>
+    </nav>
     );
 }
 export default Nav;
