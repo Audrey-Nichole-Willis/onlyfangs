3fec5f12fbcb71c3775402652d6d9ac023a983e8
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-07-11 16:07:42 -0400

    added an image to the header and wrote css that mkade the title appear on a yellow post it note

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Nav from "./Nav";
+import "./header.css";
+
 function Header(){
-    return(
-        <Nav/>
-    )
+    return (
+      <div className="header">
+        <Nav />
+        <div className="header-main">
+          <div class="paper">
+            <div class="pin">
+              <div class="shadow"></div>
+              <div class="metal"></div>
+              <div class="bottom-circle"></div>
+            </div>
+            <p><span className="header-title">OnlyFangs</span><br/>The one-stop-shop<br/> for all your exotic pet needs!</p>
+          </div>
+        </div>
+      </div>
+    );
 }
 export default Header;
