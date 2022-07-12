b1c5fb01b0579c294c414915c9ce74a359188d72
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-07-12 13:45:04 -0400

    styled the services bar for mobile, a flex column where the service items take up 100% of the element

--- src/Components/WhatWeOffer/WhatWeOffer.js
+++ src/Components/WhatWeOffer/WhatWeOffer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./what-we-offer.css";
 
 function WhatWeOffer() {
   return (
@@ -7,25 +8,25 @@ function WhatWeOffer() {
         WIth this services and help site for exotic pet owners you have access
         to our large database of verified clients including:
       </p>
-      <div className="services-box d-flex justify-content-between align-items-center">
-        <div className="service">
-          <i className="fa-solid fa-square-check"></i>
+      <div className="services-box d-flex justify-content-between align-items-center flex-column">
+        <div className="service w-100 mb-3 d-flex flex-column justify-content-center  align-items-center">
+          <i className="fa-solid fa-square-check mb-3"></i>
           <p>Reputable Breeders</p>
         </div>
-        <div className="service">
-          <i className="fa-solid fa-gear"></i>
+        <div className="service w-100 mb-3 d-flex flex-column justify-content-center align-items-center">
+          <i className="fa-solid fa-gear mb-3"></i>
           <p>Quality Craftsman</p>
         </div>
-        <div className="service">
-          <i className="fa-solid fa-building-circle-check"></i>
+        <div className="service w-100 mb-3 d-flex flex-column justify-content-center align-items-center ">
+          <i className="fa-solid fa-building-circle-check mb-3"></i>
           <p>Verified Businesses</p>
         </div>
-        <div className="service">
-          <i className="fa-solid fa-bag-shopping"></i>
+        <div className="service w-100 mb-3 d-flex flex-column justify-content-center align-items-center">
+          <i className="fa-solid fa-bag-shopping mb-3"></i>
           <p>Custom Supplies</p>
         </div>
-        <div className="service">
-          <i className="fa-solid fa-award"></i>
+        <div className="service w-100 mb-3 d-flex flex-column justify-content-center align-items-center">
+          <i className="fa-solid fa-award mb-3"></i>
           <p>Trusted Professionals</p>
         </div>
       </div>
